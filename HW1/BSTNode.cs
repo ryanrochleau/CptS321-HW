63e6d4a10d78e3e7546427b043b6c1ba744f7396
rrochlea <ryan.rochleau@wsu.edu>
2021-02-04 22:04:12 -0800

    Added BST and BSTNode classes

---
+++ HW1/BSTNode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW1
+{
+    /// <summary>
+    /// The class that represents a node
+    /// </summary>
+    class BSTNode
+    {
+        private int data;
+        public BSTNode leftNode;
+        public BSTNode rightNode;
+    }
+}
---
+++ HW1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HW1
+{
+    class Program
+    {
+        /// <summary>
+        /// Main function of the program
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            string inputString;
+
+            inputString = Console.ReadLine();
+
+            string[] subs = inputString.Split(' ');
+            int[] inputIntegers = Array.ConvertAll(subs, int.Parse);
+
+        }
+    }
+}
