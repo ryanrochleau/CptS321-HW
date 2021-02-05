3f07410e18447b4369f191b9cf1720b12b77990e
rrochlea <ryan.rochleau@wsu.edu>
2021-02-05 09:41:26 -0800

    Fixed BSTNode StyleCop errors

--- HW1/BSTNode.cs
+++ HW1/BSTNode.cs
@@ -14,9 +14,10 @@
         private BSTNode rightNode;
 
         /// <summary>
+        /// Initializes a new instance of the <see cref="BSTNode"/> class.
         /// Constructor for BSTNode.
         /// </summary>
-        /// <param name="inputData">An integer</param>
+        /// <param name="inputData">An integer.</param>
         public BSTNode(int inputData)
         {
             this.data = inputData;
@@ -25,10 +26,10 @@
         }
 
         /// <summary>
-        /// Getter for integer data
+        /// Getter for integer data.
         /// </summary>
         /// <returns>
-        /// Returns the integer data in the node
+        /// Returns the integer data in the node.
         /// </returns>
         public int GetData()
         {
@@ -36,10 +37,10 @@
         }
 
         /// <summary>
-        /// Getter for the left BSTNode
+        /// Getter for the left BSTNode.
         /// </summary>
         /// <returns>
-        /// Returns the left BSTNode
+        /// Returns the left BSTNode.
         /// </returns>
         public BSTNode GetLeftNode()
         {
@@ -47,10 +48,10 @@
         }
 
         /// <summary>
-        /// Getter for the right BSTNode
+        /// Getter for the right BSTNode.
         /// </summary>
         /// <returns>
-        /// Returns the right BSTNode
+        /// Returns the right BSTNode.
         /// </returns>
         public BSTNode GetRightNode()
         {
@@ -58,27 +59,27 @@
         }
 
         /// <summary>
-        /// Setter for the integer data
+        /// Setter for the integer data.
         /// </summary>
-        /// <param name="inputData">An integer</param>
+        /// <param name="inputData">An integer.</param>
         public void SetData(int inputData)
         {
             this.data = inputData;
         }
 
         /// <summary>
-        /// Setter for the left BSTNode
+        /// Setter for the left BSTNode.
         /// </summary>
-        /// <param name="inputNode">A BSTNode</param>
+        /// <param name="inputNode">A BSTNode.</param>
         public void SetLeftNode(BSTNode inputNode)
         {
             this.leftNode = inputNode;
         }
 
         /// <summary>
-        /// Setter for the right BSTNode
+        /// Setter for the right BSTNode.
         /// </summary>
-        /// <param name="inputNode">A BSTNode</param>
+        /// <param name="inputNode">A BSTNode.</param>
         public void SetRightNode(BSTNode inputNode)
         {
             this.rightNode = inputNode;
