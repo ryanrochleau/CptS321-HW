75025b37d1d7f9a5d14814b62ccebdeb071394f4
rrochlea <ryan.rochleau@wsu.edu>
2021-02-12 15:44:58 -0800

    Commenting inital functions provided.

--- HW2/HW2/Form1.cs
+++ HW2/HW2/Form1.cs
@@ -1,30 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace HW2
+﻿namespace HW2
 {
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// Windows form class which handles all form operations.
+    /// </summary>
     public partial class Form1 : Form
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Form1"/> class.
+        /// Constructor for the form.
+        /// </summary>
         public Form1()
         {
-            InitializeComponent();
+            this.InitializeComponent();
         }
 
+        /// <summary>
+        /// Function that executs as the form loads. Will handle
+        /// printing all of the relevent information for the
+        /// homework assignment.
+        /// </summary>
+        /// <param name="sender">An object value.</param>
+        /// <param name="e">An EventArgs value.</param>
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Windows provided function for textbox change.
+        /// </summary>
+        /// <param name="sender">An object value.</param>
+        /// <param name="e">An EventArgs value.</param>
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            
         }
     }
 }
---
+++ HW2Tests/HW2Tests/HW2Tests.cs
@@ -0,0 +1,43 @@
+﻿namespace HW2.Tests
+{
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Tests for HW2 functions.
+    /// </summary>
+    public class HW2Tests
+    {
+        /// <summary>
+        /// Initial setup function.
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        /// <summary>
+        /// Tests the hashset implementation.
+        /// </summary>
+        [Test]
+        public void HashSetTest()
+        {
+            Assert.Pass();
+        }
+
+        /// <summary>
+        /// Tests the O(1) storage implementation.
+        /// </summary>
+        public void BigOOneTest()
+        {
+            Assert.Pass();
+        }
+
+        /// <summary>
+        /// Tests the sorted list implementation.
+        /// </summary>
+        public void SortedTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
