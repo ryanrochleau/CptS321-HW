14e5da687010b6b9f8b249bf4f530cbd7ca56cd0
rrochlea <ryan.rochleau@wsu.edu>
2021-02-12 16:20:47 -0800

    Finished writing initial test cases.

---
+++ HW2/HW2/ThreeDistinct.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW2
+{
+    class ThreeDistinct
+    {
+    }
+}
--- HW2Tests/HW2Tests/HW2Tests.cs
+++ HW2Tests/HW2Tests/HW2Tests.cs
@@ -21,6 +21,17 @@
         [Test]
         public void HashSetTest()
         {
+            List<int> emptyList = new List<int>();
+            List<int> oneDistinctList = new List<int> { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+
+            ThreeDistinct firstDistinct = new ThreeDistinct();
+            ThreeDistinct firstDistinctEmpty = new ThreeDistinct(emptyList);
+            ThreeDistinct firstDistinctOne = new ThreeDistinct(oneDistinctList);
+
+            Assert.AreEqual(firstDistinct.HashSetMethod(), (firstDistinct.GetList()).Distinct());
+            Assert.AreEqual(firstDistinctEmpty.HashSetMethod(), (firstDistinct.GetList()).Distinct());
+            Assert.AreEqual(firstDistinctOne.HashSetMethod(), (firstDistinct.GetList()).Distinct());
+
             Assert.Pass();
         }
 
@@ -29,6 +40,17 @@
         /// </summary>
         public void BigOOneTest()
         {
+            List<int> emptyList = new List<int>();
+            List<int> oneDistinctList = new List<int> { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+
+            ThreeDistinct secondDistinct = new ThreeDistinct();
+            ThreeDistinct secondDistinctEmpty = new ThreeDistinct(emptyList);
+            ThreeDistinct secondDistinctOne = new ThreeDistinct(oneDistinctList);
+
+            Assert.AreEqual(secondDistinct.BigOOneMethod(), (secondDistinct.GetList()).Distinct());
+            Assert.AreEqual(secondDistinctEmpty.BigOOneMethod(), (secondDistinct.GetList()).Distinct());
+            Assert.AreEqual(secondDistinctOne.BigOOneMethod(), (secondDistinct.GetList()).Distinct());
+
             Assert.Pass();
         }
 
@@ -37,6 +59,17 @@
         /// </summary>
         public void SortedTest()
         {
+            List<int> emptyList = new List<int>();
+            List<int> oneDistinctList = new List<int> { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+
+            ThreeDistinct thirdDistinct = new ThreeDistinct();
+            ThreeDistinct thirdDistinctEmpty = new ThreeDistinct(emptyList);
+            ThreeDistinct thirdDistinctOne = new ThreeDistinct(oneDistinctList);
+
+            Assert.AreEqual(thirdDistinct.SortedMethod(), (thirdDistinct.GetList()).Distinct());
+            Assert.AreEqual(thirdDistinctEmpty.SortedMethod(), (thirdDistinct.GetList()).Distinct());
+            Assert.AreEqual(thirdDistinctOne.SortedMethod(), (thirdDistinct.GetList()).Distinct());
+
             Assert.Pass();
         }
     }
