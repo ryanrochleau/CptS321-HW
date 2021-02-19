6702d5e36e14bc9fc2ced3b73855d16dcdc12d9e
rrochlea <ryan.rochleau@wsu.edu>
2021-02-19 09:22:35 -0800

    Created intial tests.

---
+++ Exam1Tests/Exam1Tests/Exam1Tests.cs
@@ -0,0 +1,49 @@
+// <copyright file="Exam1Tests.cs" company="Ryan Rochleau">
+// Copyright (c) Ryan Rochleau. All rights reserved.
+// </copyright>
+
+namespace Exam1Tests
+{
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Tests for the exam.
+    /// </summary>
+    public class Exam1Tests
+    {
+        /// <summary>
+        /// Initial setup function.
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        /// <summary>
+        /// Tests the search function.
+        /// </summary>
+        [Test]
+        public void SearchTest()
+        {
+            Assert.Pass();
+        }
+
+        /// <summary>
+        /// Tests the save search function.
+        /// </summary>
+        [Test]
+        public void SaveSearchTest()
+        {
+            Assert.Pass();
+        }
+
+        /// <summary>
+        /// Tests the restock function.
+        /// </summary>
+        [Test]
+        public void RestockTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
