9b37fcbb32742dfdc5fad0d51865854ba5d057b1
rrochlea <ryan.rochleau@wsu.edu>
2021-02-26 16:49:48 -0800

    Completed FibonnacciTextReader tests.

--- HW3Tests/HW3Tests/HW3Tests.cs
+++ HW3Tests/HW3Tests/HW3Tests.cs
@@ -22,7 +22,18 @@ namespace HW3Tests
         [Test]
         public void FibonacciTest()
         {
-            Form1 testForm = new Form1();
+            FibonacciTextReader testFibTextReaderTwenty = new FibonacciTextReader(20);
+            FibonacciTextReader testFibTextReaderFive = new FibonacciTextReader(5);
+            FibonacciTextReader testFibTextReaderThirteen = new FibonacciTextReader(13);
+
+            string firstTwentyFibNumbers = "1: 0\r\n2: 1\r\n3: 2\r\n4: 3\r\n5: 5\r\n6: 8\r\n7: 13\r\n8: 21\r\n9: 34\r\n10: 55\r\n11: 89\r\n12: 144\r\n13: 233\r\n14: 377\r\n14: 377\r\n"
+                + "15: 610\r\n16: 987\r\n17: 1597\r\n18: 2584\r\n19: 4184\r\n20: 6765\r\n21: 10946\r\n22: 17711\r\n23: 28657\r\n24: 46368\r\n25: 46368\r\n";
+            string firstFiveFibNumbers = "1: 0\r\n2: 1\r\n3: 2\r\n4: 3\r\n5: 5\r\n";
+            string firstThirteenNumbers = "1: 0\r\n2: 1\r\n3: 2\r\n4: 3\r\n5: 5\r\n6: 8\r\n7: 13\r\n8: 21\r\n9: 34\r\n10: 55\r\n11: 89\r\n12: 144\r\n13: 233\r\n";
+
+            Assert.AreEqual(firstTwentyFibNumbers, testFibTextReaderTwenty.ReadToEnd());
+            Assert.AreEqual(firstTwentyFibNumbers, testFibTextReaderFive.ReadToEnd());
+            Assert.AreEqual(firstTwentyFibNumbers, testFibTextReaderThirteen.ReadToEnd());
             Assert.Pass();
         }
 
