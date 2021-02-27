ef3183a5f9461306477bea0789635081b53886c9
rrochlea <ryan.rochleau@wsu.edu>
2021-02-26 16:14:23 -0800

    Added initial HW3 project.

---
+++ HW3Tests/HW3Tests/HW3Tests.cs
@@ -0,0 +1,45 @@
+using NUnit.Framework;
+
+namespace HW3Tests
+{
+    /// <summary>
+    /// Tests for homework 3.
+    /// </summary>
+    public class HW3Tests
+    {
+        /// <summary>
+        /// Initial setup function.
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        /// <summary>
+        /// Tests the fibonacci reader.
+        /// </summary>
+        [Test]
+        public void FibonacciTest()
+        {
+            Assert.Pass();
+        }
+
+        /// <summary>
+        /// Tests loading from a file.
+        /// </summary>
+        [Test]
+        public void LoadFromFileTest()
+        {
+            Assert.Pass();
+        }
+
+        /// <summary>
+        /// Tests saving to a file.
+        /// </summary>
+        [Test]
+        public void SaveFileTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
