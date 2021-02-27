9d4372ac9edffc67680b9a4df07f3ae5ea78159b
rrochlea <ryan.rochleau@wsu.edu>
2021-02-26 16:50:51 -0800

    Corrected Typo in Fibonacci tests.

--- HW3Tests/HW3Tests/HW3Tests.cs
+++ HW3Tests/HW3Tests/HW3Tests.cs
@@ -29,11 +29,11 @@ namespace HW3Tests
             string firstTwentyFibNumbers = "1: 0\r\n2: 1\r\n3: 2\r\n4: 3\r\n5: 5\r\n6: 8\r\n7: 13\r\n8: 21\r\n9: 34\r\n10: 55\r\n11: 89\r\n12: 144\r\n13: 233\r\n14: 377\r\n14: 377\r\n"
                 + "15: 610\r\n16: 987\r\n17: 1597\r\n18: 2584\r\n19: 4184\r\n20: 6765\r\n21: 10946\r\n22: 17711\r\n23: 28657\r\n24: 46368\r\n25: 46368\r\n";
             string firstFiveFibNumbers = "1: 0\r\n2: 1\r\n3: 2\r\n4: 3\r\n5: 5\r\n";
-            string firstThirteenNumbers = "1: 0\r\n2: 1\r\n3: 2\r\n4: 3\r\n5: 5\r\n6: 8\r\n7: 13\r\n8: 21\r\n9: 34\r\n10: 55\r\n11: 89\r\n12: 144\r\n13: 233\r\n";
+            string firstThirteenFibNumbers = "1: 0\r\n2: 1\r\n3: 2\r\n4: 3\r\n5: 5\r\n6: 8\r\n7: 13\r\n8: 21\r\n9: 34\r\n10: 55\r\n11: 89\r\n12: 144\r\n13: 233\r\n";
 
             Assert.AreEqual(firstTwentyFibNumbers, testFibTextReaderTwenty.ReadToEnd());
-            Assert.AreEqual(firstTwentyFibNumbers, testFibTextReaderFive.ReadToEnd());
-            Assert.AreEqual(firstTwentyFibNumbers, testFibTextReaderThirteen.ReadToEnd());
+            Assert.AreEqual(firstFiveFibNumbers, testFibTextReaderFive.ReadToEnd());
+            Assert.AreEqual(firstThirteenFibNumbers, testFibTextReaderThirteen.ReadToEnd());
             Assert.Pass();
         }
 
