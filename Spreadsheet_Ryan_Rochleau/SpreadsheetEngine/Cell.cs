40e80ab6f1fec2b09836a12dd20eecf878526f2d
rrochlea <ryan.rochleau@wsu.edu>
2021-03-05 17:50:16 -0800

    Added initial spreadsheet engine and class Cell

---
+++ Spreadsheet_Ryan_Rochleau/SpreadsheetEngine/Cell.cs
@@ -0,0 +1,12 @@
+﻿// <copyright file="Cell.cs" company="Ryan Rochleau">
+// Copyright (c) Ryan Rochleau. All rights reserved.
+// </copyright>
+
+using System;
+
+namespace SpreadsheetEngine
+{
+    public class Cell
+    {
+    }
+}
