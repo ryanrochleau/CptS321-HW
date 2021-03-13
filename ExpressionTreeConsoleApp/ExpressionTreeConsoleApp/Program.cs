c8588b892b79c3595080b3df43b9b7ac45dc8413
rrochlea <ryan.rochleau@wsu.edu>
2021-03-12 21:42:29 -0800

    Tests passing, adding a console app.

---
+++ ExpressionTreeConsoleApp/ExpressionTreeConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="Program.cs" company="Ryan Rochleau">
+// Copyright (c) Ryan Rochleau. All rights reserved.
+// </copyright>
+
+using System;
+using CptS321;
+
+namespace ExpressionTreeConsoleApp
+{
+    /// <summary>
+    /// Main program class.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Main function.
+        /// </summary>
+        /// <param name="args">Arguments provided to main at runtime.</param>
+        public void Main(string[] args)
+        {
+
+        }
+    }
+}
