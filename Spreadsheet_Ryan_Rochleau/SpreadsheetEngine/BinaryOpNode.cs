a410617d608dfc9707d1e08533d5476842a69556
rrochlea <ryan.rochleau@wsu.edu>
2021-03-12 18:44:03 -0800

    Completed BinaryOpNode implementation.

--- Spreadsheet_Ryan_Rochleau/SpreadsheetEngine/BinaryOpNode.cs
+++ Spreadsheet_Ryan_Rochleau/SpreadsheetEngine/BinaryOpNode.cs
@@ -9,5 +9,37 @@ namespace CptS321
     /// </summary>
     internal class BinaryOpNode : Node
     {
+        /// <summary>
+        /// Gets or sets the operator of this node.
+        /// </summary>
+        private char Operator { get; set; }
+
+        /// <summary>
+        /// Gets or sets the left node.
+        /// </summary>
+        private Node LeftNode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the right node.
+        /// </summary>
+        private Node RightNode { get; set; }
+
+        /// <summary>
+        /// Evaluates the expression tree rooted by
+        /// this node.
+        /// </summary>
+        /// <returns>A double value which is the value of the expression tree
+        /// rooted by this node.</returns>
+        public override double Evaluate()
+        {
+            switch (this.Operator)
+            {
+                case '+': return this.LeftNode.Evaluate() + this.RightNode.Evaluate();
+                case '-': return this.LeftNode.Evaluate() - this.RightNode.Evaluate();
+                case '*': return this.LeftNode.Evaluate() * this.RightNode.Evaluate();
+                case '/': return this.LeftNode.Evaluate() / this.RightNode.Evaluate();
+                default: throw new ArgumentException(string.Format("The operator {0} is not a valid operator.", this.Operator), "Operator");
+            }
+        }
     }
 }
