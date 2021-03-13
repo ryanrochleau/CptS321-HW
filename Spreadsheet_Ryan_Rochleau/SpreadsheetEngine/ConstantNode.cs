b259be0ab2de3a777fd620f075711557d5a0a566
rrochlea <ryan.rochleau@wsu.edu>
2021-03-12 18:19:09 -0800

    Completed ConstantNode.

--- Spreadsheet_Ryan_Rochleau/SpreadsheetEngine/ConstantNode.cs
+++ Spreadsheet_Ryan_Rochleau/SpreadsheetEngine/ConstantNode.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace SpreadsheetEngine
+namespace CptS321
 {
     /// <summary>
     /// Node representing a contant value.
     /// </summary>
     internal class ConstantNode : Node
     {
+        /// <summary>
+        /// Gets or sets the value that the constant node is set to which is
+        /// a double.
+        /// </summary>
+        private double ConstantValue { get; set; }
     }
 }
--- Spreadsheet_Ryan_Rochleau/SpreadsheetEngine/Node.cs
+++ Spreadsheet_Ryan_Rochleau/SpreadsheetEngine/Node.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace SpreadsheetEngine
+namespace CptS321
 {
     /// <summary>
     /// Base class for a node in the expression tree.
