20b5d13fab5d111956cb284501ff5a83f8f253f7
rrochlea <ryan.rochleau@wsu.edu>
2021-03-12 19:23:21 -0800

    Completed VariableNode implementation.

--- Spreadsheet_Ryan_Rochleau/SpreadsheetEngine/VariableNode.cs
+++ Spreadsheet_Ryan_Rochleau/SpreadsheetEngine/VariableNode.cs
@@ -9,5 +9,23 @@ namespace CptS321
     /// </summary>
     internal class VariableNode : Node
     {
+        /// <summary>
+        /// Gets or sets the name of the variable as a string.
+        /// </summary>
+        private string VariableName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value of the variable as a double.
+        /// </summary>
+        private double VariableValue { get; set; }
+
+        /// <summary>
+        /// Evaluated the variable node.
+        /// </summary>
+        /// <returns>Returns the value of node.</returns>
+        public override double Evaluate()
+        {
+            return this.VariableValue;
+        }
     }
 }
