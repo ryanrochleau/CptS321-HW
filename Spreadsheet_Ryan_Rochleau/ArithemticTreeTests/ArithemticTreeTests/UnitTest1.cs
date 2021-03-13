97eb0c4782c55c034248ad3d33be6403f32cd547
rrochlea <ryan.rochleau@wsu.edu>
2021-03-12 17:57:07 -0800

    TDD: Added tests for Add, Subtract, Multiply, and Divide.

--- Spreadsheet_Ryan_Rochleau/ArithemticTreeTests/ArithemticTreeTests/UnitTest1.cs
+++ Spreadsheet_Ryan_Rochleau/ArithemticTreeTests/ArithemticTreeTests/UnitTest1.cs
@@ -10,8 +10,83 @@ namespace ArithemticTreeTests
         }
 
         [Test]
-        public void Test1()
+        public void TestAddition()
         {
+            ExpressionTree testTree;
+
+            testTree = new ExpressionTree("1+3+5+7+9");
+            Assert.AreEqual(testTree.Evaluate(), 25);
+
+            testTree = new ExpressionTree("A+B+C");
+            testTree.AddVariable("A", 1);
+            testTree.AddVariable("B", 2);
+            testTree.AddVariable("C", 3);
+            Assert.AreEqual(testTree.Evaluate(), 6);
+
+            testTree = new ExpressionTree("1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1");
+            Assert.AreEqual(testTree.Evaluate(), 40);
+
+
+            Assert.Pass();
+        }
+
+        public void TestSubtraction()
+        {
+            ExpressionTree testTree;
+
+            testTree = new ExpressionTree("9-6");
+            Assert.AreEqual(testTree.Evaluate(), 3);
+
+            testTree = new ExpressionTree("A-B-C");
+            testTree.AddVariable("A", 1);
+            testTree.AddVariable("B", 2);
+            testTree.AddVariable("C", 3);
+            Assert.AreEqual(testTree.Evaluate(), -4);
+
+            testTree = new ExpressionTree("1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1");
+            Assert.AreEqual(testTree.Evaluate(), -40);
+
+
+            Assert.Pass();
+        }
+
+        public void TestMultiplication()
+        {
+            ExpressionTree testTree;
+
+            testTree = new ExpressionTree("9*6");
+            Assert.AreEqual(testTree.Evaluate(), 54);
+
+            testTree = new ExpressionTree("A*B*C");
+            testTree.AddVariable("A", 1);
+            testTree.AddVariable("B", 2);
+            testTree.AddVariable("C", 4);
+            Assert.AreEqual(testTree.Evaluate(), 8);
+
+            testTree = new ExpressionTree("1*1*1*1*1*1*1*1*1*1*1*1*1*1*1*1*1*1*1*1*1*1*1*1*1*1*1*1*1*1*1*1*1*1*1*1*1*1*1*1*1*1*1*1*1*1*1*1*1*1*1");
+            Assert.AreEqual(testTree.Evaluate(), 1);
+
+
+            Assert.Pass();
+        }
+
+        public void TestDivision()
+        {
+            ExpressionTree testTree;
+
+            testTree = new ExpressionTree("9/3");
+            Assert.AreEqual(testTree.Evaluate(), 3);
+
+            testTree = new ExpressionTree("C/B/A");
+            testTree.AddVariable("A", 1);
+            testTree.AddVariable("B", 2);
+            testTree.AddVariable("C", 4);
+            Assert.AreEqual(testTree.Evaluate(), 2);
+
+            testTree = new ExpressionTree("1/1/1/1/1/1/1/1/1/1/1/1/1/1/1/1/1/1/1/1/1/1/1/1/1/1/1/1/1/1/1/1/1/1/1/1/1/1/1/1/1/1/1/1/1/1/1/1/1/1/1/1");
+            Assert.AreEqual(testTree.Evaluate(), 1);
+
+
             Assert.Pass();
         }
     }
