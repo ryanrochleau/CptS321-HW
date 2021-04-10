8b797797e5911c0cc9edb79706eedcbd2e0c9069
rrochlea <ryan.rochleau@wsu.edu>
2021-04-09 18:10:42 -0700

    Added 2 classes and an interface for handling the undoing/redoing of text and color changes.

---
+++ Spreadsheet_Ryan_Rochleau/SpreadsheetEngine/ColorUndoRedo.cs
@@ -0,0 +1,71 @@
+﻿// <copyright file="ColorUndoRedo.cs" company="Ryan Rochleau">
+// Copyright (c) Ryan Rochleau. All rights reserved.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CptS321
+{
+    /// <summary>
+    /// Class for undo/redo of colors. Uses a list of cells since
+    /// we can select multiple and change their colors all at once.
+    /// </summary>
+    public class ColorUndoRedo : IUndoRedoInterface
+    {
+        /// <summary>
+        /// New color of all the cells.
+        /// </summary>
+        private uint newColor;
+
+        /// <summary>
+        /// Previous colors of the cells.
+        /// </summary>
+        private List<uint> prevColors;
+
+        /// <summary>
+        /// List of all cells that were changed.
+        /// </summary>
+        private List<Cell> cells;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ColorUndoRedo"/> class.
+        /// </summary>
+        /// <param name="cells">List of all cells being changed.</param>
+        /// <param name="prevColors">Previous colors of the cells.</param>
+        /// <param name="newColor">New color for the cells.</param>
+        public ColorUndoRedo(List<Cell> cells, List<uint> prevColors, uint newColor)
+        {
+            this.cells = cells;
+            this.prevColors = prevColors;
+            this.newColor = newColor;
+        }
+
+        /// <summary>
+        /// Implements undo for color.
+        /// </summary>
+        public void InterfaceUndo()
+        {
+            // Undoing a change means reverting the color of each cell
+            // back to its original color.
+            for (int i = 0; i < this.cells.Count; i++)
+            {
+                this.cells[i].SetColor(this.prevColors[i]);
+            }
+        }
+
+        /// <summary>
+        /// Implements redo for color.
+        /// </summary>
+        public void InterfaceRedo()
+        {
+            // Redoing means setting the cells original colors back to
+            // the new color.
+            foreach (Cell oldCell in this.cells)
+            {
+                oldCell.SetColor(this.newColor);
+            }
+        }
+    }
+}
---
+++ Spreadsheet_Ryan_Rochleau/SpreadsheetEngine/IUndoRedoInterface.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="UndoRedoInterface.cs" company="Ryan Rochleau">
+// Copyright (c) Ryan Rochleau. All rights reserved.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CptS321
+{
+    /// <summary>
+    /// Interface for undo/redo.
+    /// </summary>
+    public interface IUndoRedoInterface
+    {
+        /// <summary>
+        /// Undo function for the interface which
+        /// the other undo/redo commands will
+        /// implement.
+        /// </summary>
+        void InterfaceUndo();
+
+        /// <summary>
+        /// Redo function for the interface which
+        /// the other undo/redo commands will
+        /// implement.
+        /// </summary>
+        void InterfaceRedo();
+    }
+}
